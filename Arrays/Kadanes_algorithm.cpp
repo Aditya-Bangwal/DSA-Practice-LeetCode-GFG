f1c7886c1c2aa2f10434bc8f6624bb0beb4283d7
Aditya-Bangwal <adityabangwal12@gmail.com>
2025-07-10 14:15:09 +0530

    Update Kadanes_algorithm.cpp

--- Arrays/Kadanes_algorithm.cpp
+++ Arrays/Kadanes_algorithm.cpp
@@ -1,1 +1,20 @@
-//kadane algo
+// Kadane's Algorithm: Maximum Subarray Sum
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    int maxSum = nums[0];
+    int curr = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        curr = max(nums[i], curr + nums[i]);
+        maxSum = max(maxSum, curr);
+    }
+    return maxSum;
+}
+
+int main() {
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    cout << "Max Subarray Sum: " << maxSubArray(nums) << endl;
+    return 0;
+}
