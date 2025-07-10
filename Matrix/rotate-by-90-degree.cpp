64e552396ddb307a43e766b3f4860ae2e7981a8c
Aditya-Bangwal <adityabangwal12@gmail.com>
2025-07-10 21:44:30 +0530

    Create rotate-by-90-degree.cpp

---
+++ Matrix/rotate-by-90-degree.cpp
@@ -0,0 +1,21 @@
+//to rotate it by 90 degrees in clockwise direction without using any extra space.
+
+//90 degree rotation is equals to transverse of matrix and then reverse each row 
+//transverse is to swap row with column and column with row
+void rotate(vector<vector<int> >& mat) {
+    // Your code goes here
+    int m=mat.size();
+    int n=mat[0].size();
+   for(int i=0;i<n-1;i++)
+   {
+       for(int j=i;j<n;j++)
+       {
+           swap(mat[i][j],mat[j][i]);
+           
+       }
+   }
+   for(int i=0;i<m;i++)
+   {
+       reverse(mat[i].begin(),mat[i].end());
+   }
+}
